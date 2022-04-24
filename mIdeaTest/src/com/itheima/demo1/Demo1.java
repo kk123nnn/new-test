7a51a95b837348f2fb11cd5b1ccd3e0b064ce8a8
JohnnyYao <yaomengtingjohnny@gmail.com>
2022-04-24 21:41:55 +1200

    第一次提交

---
+++ mIdeaTest/src/com/itheima/demo1/Demo1.java
@@ -0,0 +1,12 @@
+package com.itheima.demo1;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        String stu;
+        int a;
+        a = 10;
+        stu = "abc";
+        System.out.println(stu);
+        System.out.println(a);
+    }
+}
