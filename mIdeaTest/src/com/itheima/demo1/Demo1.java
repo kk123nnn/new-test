7de2c8c20a0ad635a9c9cfba89ab3e2e63026118
JohnnyYao <yaomengtingjohnny@gmail.com>
2022-05-02 22:45:49 +1200

    第一次提交

--- mIdeaTest/src/com/itheima/demo1/Demo1.java
+++ mIdeaTest/src/com/itheima/demo1/Demo1.java
@@ -1,12 +1,24 @@
 package com.itheima.demo1;
 
+import java.util.Scanner;
+
 public class Demo1 {
     public static void main(String[] args) {
-        String stu;
-        int a;
-        a = 10;
-        stu = "abc";
-        System.out.println(stu);
-        System.out.println(a);
+        int[] arr = new int[6];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("请第" + (i + 1) + "个评委打分");
+            int temp = sc.nextInt();
+            if (temp >= 0 && temp <= 100) {
+                arr[i] = temp;
+            } else {
+                System.out.println("输入不符合规范，请重新输入");
+                i--;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+//        System.out.println(index);
     }
 }
