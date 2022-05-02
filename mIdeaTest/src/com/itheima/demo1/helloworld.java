fe6178f483aba33cfcff5d8b572f8b050a242b86
JohnnyYao <yaomengtingjohnny@gmail.com>
2022-05-02 22:57:32 +1200

    Revert "第3次提交"
    This reverts commit d6c7ba25

--- mIdeaTest/src/com/itheima/demo1/helloworld.java
+++ mIdeaTest/src/com/itheima/demo1/helloworld.java
@@ -5,6 +5,5 @@ public class helloworld {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
     }
 }
